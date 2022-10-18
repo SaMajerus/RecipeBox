59960ad250dd636da082511fd81759754cc2a846
Samuel Majerus <ladolego@gmail.com>
2022-10-18 11:09:25 -0700

    Updates README, 'RecipeController.cs'. Uncomments line 9 of 'Register.cshtml'.
    Co-authored-by: Joseph Jackson <josephjacksonw@gmail.com>

--- README_Refactoring-Files__ToDo.md
+++ README_Refactoring-Files__ToDo.md
@@ -3,3 +3,7 @@ models - appUser, category, recipe, categoryrecipe, context, and DesignDB (delet
 
 category - name (its type of food)
 recipe - name(can leave as description if we don't want to change stuff), ingredients, instructions, possibly a rating
+
+
+so it should work, maybe
+the views probably don't work bc we changed wha
--- RecipeBox/Controllers/RecipesController.cs
+++ RecipeBox/Controllers/RecipesController.cs
@@ -3,71 +3,124 @@ using RecipeBox.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using Microsoft.AspNetCore.Authorization;  //"Allows us to actually authorize users" (Lsn 9).
+using Microsoft.AspNetCore.Identity;  //"[Allows] our controller [to] interact with users from the database" (Lsn 9). 
+using System.Threading.Tasks;  //"[Is] necessary to call async methods" (Lsn 9). 
+using System.Security.Claims;  /* "[I]mportant for using claim based authorization. A claim is a form of user identification. It states who a user is, not what the user can actually do. While the user identification itself doesn't authorize a user to do anything, it is necessary to first identify a user (through a claim) in order to determine whether they should be authorized" (Lsn 9). */
 
 namespace RecipeBox.Controllers
 {
-  public class CategoriesController : Controller
+  [Authorize]  //This attribute "allows access to the 'RecipesController' only if a user is logged in" (Lsn 9). 
+  public class RecipesController : Controller
   {
     private readonly RecipeBoxContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;  //new line 
 
-    public CategoriesController(RecipeBoxContext db)
+    //updated constructor
+    public RecipesController(UserManager<ApplicationUser> userManager, RecipeBoxContext db)
     {
+      _userManager = userManager;
       _db = db;
     }
 
-    public ActionResult Index()
+    public async Task<ActionResult> Index()
     {
-      List<Category> model = _db.Categories.ToList();
-      return View(model);
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var userRecipes = _db.Recipes.Where(entry => entry.User.Id == currentUser.Id).ToList();
+      return View(userRecipes);
     }
 
     public ActionResult Create()
     {
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
       return View();
     }
 
     [HttpPost]
-    public ActionResult Create(Category category)
+    public async Task<ActionResult> Create(Recipe recipe, int CategoryId)
     {
-      _db.Categories.Add(category);
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      recipe.User = currentUser;
+      _db.Recipes.Add(recipe);
+      _db.SaveChanges();
+      if (CategoryId != 0)
+      {
+        _db.CategoryRecipe.Add(new CategoryRecipe() { CategoryId = CategoryId, RecipeId = recipe.RecipeId });
+      }
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
 
     public ActionResult Details(int id)
     {
-      var thisCategory = _db.Categories
-        .Include(category => category.JoinEntities)
-        .ThenInclude(join => join.Item)
-        .FirstOrDefault(category => category.CategoryId == id);
-      return View(thisCategory);
+      var thisRecipe = _db.Recipes
+        .Include(recipe => recipe.JoinEntities)
+        .ThenInclude(join => join.Category)
+        .FirstOrDefault(recipe => recipe.RecipeId == id);
+      return View(thisRecipe);
     }
 
     public ActionResult Edit(int id)
     {
-      Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
-      return View(thisCategory);
+      Recipe thisRecipe = _db.Recipes.FirstOrDefault(recipe => recipe.RecipeId == id);
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View(thisRecipe);
     }
 
     [HttpPost]
-    public ActionResult Edit(Category category)
+    public ActionResult Edit(Recipe recipe, int CategoryId)
     {
-      _db.Entry(category).State = EntityState.Modified;
+      if (CategoryId != 0)
+      {
+        _db.CategoryRecipe.Add(new CategoryRecipe() { CategoryId = CategoryId, RecipeId = recipe.RecipeId });
+      }
+      _db.Entry(recipe).State = EntityState.Modified;
       _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddCategory(int id)
+    {
+      var thisRecipe = _db.Recipes.FirstOrDefault(recipe => recipe.RecipeId == id);
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View(thisRecipe);
+    }
+
+    [HttpPost]
+    public ActionResult AddCategory(Recipe recipe, int CategoryId)
+    {
+      if (CategoryId != 0)
+      {
+        _db.CategoryRecipe.Add(new CategoryRecipe() { CategoryId = CategoryId, RecipeId = recipe.RecipeId });
+        _db.SaveChanges();
+      }
       return RedirectToAction("Index");
     }
 
     public ActionResult Delete(int id)
     {
-      Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
-      return View(thisCategory);
+      Recipe thisRecipe = _db.Recipes.FirstOrDefault(recipe => recipe.RecipeId == id);
+      return View(thisRecipe);
     }
 
     [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
     {
-      Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
-      _db.Categories.Remove(thisCategory);
+      Recipe thisRecipe = _db.Recipes.FirstOrDefault(recipe => recipe.RecipeId == id);
+      _db.Recipes.Remove(thisRecipe);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteCategory(int joinId)
+    {
+      var joinEntry = _db.CategoryRecipe.FirstOrDefault(entry => entry.CategoryRecipeId == joinId);
+      _db.CategoryRecipe.Remove(joinEntry);
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
