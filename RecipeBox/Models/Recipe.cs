7d7be9b601e5281ca2ccc749767d0f792c40f920
Samuel Majerus <ladolego@gmail.com>
2022-10-21 09:50:15 -0700

    Adds a single line-comment for informational purposes

--- RecipeBox/Models/Recipe.cs
+++ RecipeBox/Models/Recipe.cs
@@ -14,7 +14,7 @@ namespace RecipeBox.Models
     public int RecipeId { get; set; }
     public string Description { get; set; }
     public int Rating { get; set; }
-    // public string Ingredients { get; set; }
+    // public string Ingredients { get; set; }  //(From before 'Ingredient' was added as its own DB table)
     public string Steps { get; set; }
     public virtual ApplicationUser User { get; set; }  //Declared as 'virtual' to allow Entity to lazy-load the property's contents. 
 
